413c5145525ecfe2059ec2bb23b622171cb7af1c
DWats <DWats@DESKTOP-AFNF5HB>
2023-10-07 03:05:39 -0500

    Add project files.

---
+++ T-72B1/Class1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using MelonLoader;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using GHPC.Weapons;
+using UnityEngine;
+using GHPC.Camera;
+using GHPC.Player;
+using GHPC.Vehicle;
+
+namespace T_72B1
+{
+    public class T72MOD:MelonMod
+    {
+        AmmoClipCodexScriptable clip_codex_3bm32;
+        AmmoType.AmmoClip clip_3bm32;
+        AmmoCodexScriptable ammo_codex_3bm32;
+        AmmoType ammo_3bm32;
+        AmmoType ammo_3bm15;
+
+        GameObject[] vic_gos;
+        GameObject gameManager;
+        CameraManager cameraManager;
+        PlayerInput playerManager;
+
+        // https://snipplr.com/view/75285/clone-from-one-object-to-another-using-reflection
+        public static void ShallowCopy(System.Object dest, System.Object src)
+        {
+            BindingFlags flags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+            FieldInfo[] destFields = dest.GetType().GetFields(flags);
+            FieldInfo[] srcFields = src.GetType().GetFields(flags);
+
+            foreach (FieldInfo srcField in srcFields)
+            {
+                FieldInfo destField = destFields.FirstOrDefault(field => field.Name == srcField.Name);
+
+                if (destField != null && !destField.IsLiteral)
+                {
+                    if (srcField.FieldType == destField.FieldType)
+                        destField.SetValue(dest, srcField.GetValue(src));
+                }
+            }
+        }
+        public static void EmptyRack(GHPC.Weapons.AmmoRack rack)
+        {
+            MethodInfo removeVis = typeof(GHPC.Weapons.AmmoRack).GetMethod("RemoveAmmoVisualFromSlot", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            PropertyInfo stored_clips = typeof(GHPC.Weapons.AmmoRack).GetProperty("StoredClips");
+            stored_clips.SetValue(rack, new List<AmmoType.AmmoClip>());
+
+            rack.SlotIndicesByAmmoType = new Dictionary<AmmoType, List<byte>>();
+
+            foreach (Transform transform in rack.VisualSlots)
+            {
+                AmmoStoredVisual vis = transform.GetComponentInChildren<AmmoStoredVisual>();
+
+                if (vis != null && vis.AmmoType != null)
+                {
+                    removeVis.Invoke(rack, new object[] { transform });
+                }
+            }
+        }
+       public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            if (sceneName == "LOADER_INITIAL" || sceneName == "MainMenu2_Scene") return;
+
+            vic_gos = GameObject.FindGameObjectsWithTag("Vehicle");
+
+            if (ammo_3bm32 == null)
+            {
+
+                foreach (AmmoCodexScriptable s in Resources.FindObjectsOfTypeAll(typeof(AmmoCodexScriptable)))
+                {
+                    if (s.AmmoType.Name == "3BM15 APFSDS-T")
+                    {
+                        ammo_3bm15 = s.AmmoType;
+                    }
+                }
+
+                // 3bm32 
+                ammo_3bm32 = new AmmoType();
+                ShallowCopy(ammo_3bm32, ammo_3bm15);
+                ammo_3bm32.Name = "3BM32 APFSDS-T";
+                ammo_3bm32.Caliber = 125;
+                ammo_3bm32.RhaPenetration = 540;
+                ammo_3bm32.MuzzleVelocity = 1700f;
+                ammo_3bm32.Mass = 6.5f;
+
+                ammo_codex_3bm32 = ScriptableObject.CreateInstance<AmmoCodexScriptable>();
+                ammo_codex_3bm32.AmmoType = ammo_3bm32;
+                ammo_codex_3bm32.name = "ammo_3bm32";
+
+                clip_3bm32 = new AmmoType.AmmoClip();
+                clip_3bm32.Capacity = 1;
+                clip_3bm32.Name = "3BM32 APFSDS-T";
+                clip_3bm32.MinimalPattern = new AmmoCodexScriptable[1];
+                clip_3bm32.MinimalPattern[0] = ammo_codex_3bm32;
+
+                clip_codex_3bm32 = ScriptableObject.CreateInstance<AmmoClipCodexScriptable>();
+                clip_codex_3bm32.name = "clip_3bm32";
+                clip_codex_3bm32.ClipType = clip_3bm32;
+            }
+
+            foreach (GameObject vic_go in vic_gos)
+            {
+                Vehicle vic = vic_go.GetComponent<Vehicle>();
+
+                if (vic == null) continue;
+
+                if (vic.FriendlyName == "T-72M1")
+                {
+
+                    gameManager = GameObject.Find("_APP_GHPC_");
+                    cameraManager = gameManager.GetComponent<CameraManager>();
+                    playerManager = gameManager.GetComponent<PlayerInput>();
+
+                    GameObject ammo_3bm32_vis = null;
+
+                    // generate visual models 
+                    if (ammo_3bm32_vis == null)
+                    {
+                        ammo_3bm32_vis = GameObject.Instantiate(ammo_3bm15.VisualModel);
+                        ammo_3bm32_vis.name = "3BM32 visual";
+                        ammo_3bm32.VisualModel = ammo_3bm32_vis;
+                        ammo_3bm32.VisualModel.GetComponent<AmmoStoredVisual>().AmmoType = ammo_3bm32;
+                        ammo_3bm32.VisualModel.GetComponent<AmmoStoredVisual>().AmmoScriptable = ammo_codex_3bm32;
+                    }
+
+                    // rename to t72b
+                    FieldInfo friendlyName = typeof(GHPC.Unit).GetField("_friendlyName", BindingFlags.NonPublic | BindingFlags.Instance);
+                    friendlyName.SetValue(vic, "T-72B");
+
+                    FieldInfo uniqueName = typeof(GHPC.Unit).GetField("_uniqueName", BindingFlags.NonPublic | BindingFlags.Instance);
+                    uniqueName.SetValue(vic, "T-72B");
+
+                    // convert ammo
+                    LoadoutManager loadoutManager = vic.GetComponent<LoadoutManager>();
+
+                    loadoutManager.LoadedAmmoTypes[0] = clip_codex_3bm32;
+
+                    for (int i = 0; i <= 2; i++)
+                    {
+                        GHPC.Weapons.AmmoRack rack = loadoutManager.RackLoadouts[i].Rack;
+                        rack.ClipTypes[0] = clip_codex_3bm32.ClipType;
+                        EmptyRack(rack);
+                    }
+
+                    loadoutManager.SpawnCurrentLoadout();
+
+                    PropertyInfo roundInBreech = typeof(AmmoFeed).GetProperty("AmmoTypeInBreech");
+                    WeaponsManager weaponsManager = vic.GetComponent<WeaponsManager>();
+                    WeaponSystemInfo mainGunInfo = weaponsManager.Weapons[0];
+                    WeaponSystem mainGun = mainGunInfo.Weapon;
+                    roundInBreech.SetValue(mainGun.Feed, null);
+
+                    MethodInfo refreshBreech = typeof(AmmoFeed).GetMethod("Start", BindingFlags.Instance | BindingFlags.NonPublic);
+                    refreshBreech.Invoke(mainGun.Feed, new object[] { });
+
+                    // update ballistics computer
+                    MethodInfo registerAllBallistics = typeof(LoadoutManager).GetMethod("RegisterAllBallistics", BindingFlags.Instance | BindingFlags.NonPublic);
+                    registerAllBallistics.Invoke(loadoutManager, new object[] { });
+                }
+            }
+        }
+    }
+}
---
+++ T-72B1/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using T_72B1;
+
+[assembly: MelonInfo(typeof(T72MOD), "T-72B", "1.0.0", "ATLAS and Schweiß")]
+[assembly: MelonGame("Radian Simulations LLC", "GHPC")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("T-72B1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("T-72B1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ffc7d7f8-1043-4a85-90a9-d8f05e9560d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
